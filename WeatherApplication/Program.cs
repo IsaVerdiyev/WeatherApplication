efea3cc7196afa6457a2cb484d9526d9f9c4fb82
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-14 19:15:28 +0400

    Added inteface IWeatherInfoGetter for services which will get list of weathers and class for this interface which will work with open weather map Api

--- WeatherApplication/Program.cs
+++ WeatherApplication/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WeatherApplication.View;
 
 namespace WeatherApplication
 {
@@ -16,7 +17,7 @@ namespace WeatherApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WeatherAppForm());
         }
     }
 }
---
+++ WeatherApplication/Services/OpenWeatherMapWeatherInfoGetter.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApplication.Model;
+
+namespace WeatherApplication.Services
+{
+    class OpenWeatherMapWeatherInfoGetter : IWeatherInfoGetter
+    {
+        string firstPartOfRequestForecast = "http://api.openweathermap.org/data/2.5/forecast?&units=metric&q=";
+
+        string apiKey;
+
+        
+
+        public List<Weather> GetWeathersOfCity(string city)
+        {
+            string response = GetResponseFromWeatherOpenMap(city);
+            return ParseJsonResponseIntoListOfWeathers(response);
+        }
+
+        public async Task<List<Weather>> GetWeathersOfCityAsync(string city)
+        {
+            string response = await getResponseFromWeatherOpenMapAsync(city);
+            return ParseJsonResponseIntoListOfWeathers(response);
+        }
+
+        string GetResponseFromWeatherOpenMap(string city)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadString($"{firstPartOfRequestForecast}{city}&APPID{apiKey}");
+            }
+        }
+
+        async Task<string> getResponseFromWeatherOpenMapAsync(string city)
+        {
+            using(WebClient client = new WebClient())
+            {
+                return await client.DownloadStringTaskAsync($"{firstPartOfRequestForecast}{city}&APPID{apiKey}");
+            }
+        }
+
+        List<Weather> ParseJsonResponseIntoListOfWeathers(string jsonResponse)
+        {
+            JObject jObj = JObject.Parse(jsonResponse);
+
+            return jObj["list"].AsParallel().Select(w => new Weather
+                {
+                    Temperature = w.SelectToken("main.temp").Value<double>(),
+                    Date = w.SelectToken("dt_txt").Value<DateTime>(),
+                    MaxTemperature = w.SelectToken("main.temp_max").Value<double>(),
+                    MinTemperature = w.SelectToken("main.temp_min").Value<double>(),
+                    Pressure = w.SelectToken("main.pressure").Value<double>(),
+                    Humidity = w.SelectToken("main.humidity").Value<double>(),
+                    WindSpeed = w.SelectToken("wind.speed").Value<double>(),
+                    IconPath = w.SelectToken("weather.icon").Value<string>()
+                }).ToList<Weather>();
+        }
+
+
+
+    }
+}
