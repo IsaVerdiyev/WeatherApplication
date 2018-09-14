32a8c77d39f59f885867b8b089481af2b37559fb
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-14 15:37:37 +0400

    Added Weather class in Model folder

---
+++ WeatherApplication/Model/CurrentWeather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Model
+{
+    class CurrentWeather
+    {
+        private double currentTemperature;
+
+        public double CurrentTemperature
+        {
+            get { return currentTemperature; }
+            set { currentTemperature = value; }
+        }
+
+    }
+}
---
+++ WeatherApplication/Model/Weather.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Model
+{
+    class Weather
+    {
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private double temperature;
+
+        public double Temperature
+        {
+            get { return temperature; }
+            set { temperature = value; }
+        }
+
+        private double pressure;
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+
+        private double humidity;
+
+        public double Humidity
+        {
+            get { return humidity; }
+            set { humidity = value; }
+        }
+
+        private double windSpeed;
+
+        public double WindSpeed
+        {
+            get { return windSpeed; }
+            set { windSpeed = value; }
+        }
+
+        private string iconPath;
+
+        public string IconPath
+        {
+            get { return iconPath; }
+            set { iconPath = value; }
+        }
+
+        private double minTemperature;
+
+        public double MinTemperature
+        {
+            get { return minTemperature; }
+            set { minTemperature = value; }
+        }
+
+        private double maxTemperature;
+
+        public double MaxTemperature
+        {
+            get { return maxTemperature; }
+            set { maxTemperature = value; }
+        }
+
+
+
+
+    }
+}
