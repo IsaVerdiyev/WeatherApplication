803fcffde3589ec6601f10ce079b0e330563bee7
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-20 11:50:17 +0400

    Added HourlyItemUserControl in subviews which will describe weather of day for specific hour

---
+++ WeatherApplication/View/SubViews/HourlyItemUserControl.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WeatherApplication.View.SubViews
+{
+    partial class HourlyItemUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.HumidityLabel = new System.Windows.Forms.Label();
+            this.WindLabel = new System.Windows.Forms.Label();
+            this.ItemTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ItemTableLayoutPanel
+            // 
+            this.ItemTableLayoutPanel.ColumnCount = 1;
+            this.ItemTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ItemTableLayoutPanel.Controls.Add(this.TemperatureLabel, 0, 1);
+            this.ItemTableLayoutPanel.Controls.Add(this.DescriptionLabel, 0, 2);
+            this.ItemTableLayoutPanel.Controls.Add(this.HumidityLabel, 0, 4);
+            this.ItemTableLayoutPanel.Controls.Add(this.WindLabel, 0, 5);
+            this.ItemTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.ItemTableLayoutPanel.Name = "ItemTableLayoutPanel";
+            this.ItemTableLayoutPanel.RowCount = 6;
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22223F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22223F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22223F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.ItemTableLayoutPanel.Size = new System.Drawing.Size(150, 150);
+            this.ItemTableLayoutPanel.TabIndex = 0;
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TemperatureLabel.Location = new System.Drawing.Point(3, 33);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(144, 33);
+            this.TemperatureLabel.TabIndex = 0;
+            this.TemperatureLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DescriptionLabel.Location = new System.Drawing.Point(3, 66);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(144, 16);
+            this.DescriptionLabel.TabIndex = 1;
+            this.DescriptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HumidityLabel
+            // 
+            this.HumidityLabel.AutoSize = true;
+            this.HumidityLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HumidityLabel.Location = new System.Drawing.Point(3, 115);
+            this.HumidityLabel.Name = "HumidityLabel";
+            this.HumidityLabel.Size = new System.Drawing.Size(144, 16);
+            this.HumidityLabel.TabIndex = 2;
+            this.HumidityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WindLabel
+            // 
+            this.WindLabel.AutoSize = true;
+            this.WindLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WindLabel.Location = new System.Drawing.Point(3, 131);
+            this.WindLabel.Name = "WindLabel";
+            this.WindLabel.Size = new System.Drawing.Size(144, 19);
+            this.WindLabel.TabIndex = 3;
+            this.WindLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HourlyItemUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ItemTableLayoutPanel);
+            this.Name = "HourlyItemUserControl";
+            this.ItemTableLayoutPanel.ResumeLayout(false);
+            this.ItemTableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel ItemTableLayoutPanel;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label HumidityLabel;
+        private System.Windows.Forms.Label WindLabel;
+    }
+}
