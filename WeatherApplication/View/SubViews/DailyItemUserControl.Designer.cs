5a4e5836daf0d6766de983b6d7bed29016813dd2
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-20 11:47:25 +0400

    Added DailyItemUserControl in subviews forlder. This item will be used in MainInfoView column which describes daily weather.

---
+++ WeatherApplication/View/SubViews/DailyItemUserControl.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WeatherApplication.View.SubViews
+{
+    partial class DailyItemUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.DesciptionLabel = new System.Windows.Forms.Label();
+            this.ItemTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ItemTableLayoutPanel
+            // 
+            this.ItemTableLayoutPanel.ColumnCount = 1;
+            this.ItemTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ItemTableLayoutPanel.Controls.Add(this.DesciptionLabel, 0, 3);
+            this.ItemTableLayoutPanel.Controls.Add(this.TemperatureLabel, 0, 2);
+            this.ItemTableLayoutPanel.Controls.Add(this.DateLabel, 0, 0);
+            this.ItemTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.ItemTableLayoutPanel.Name = "ItemTableLayoutPanel";
+            this.ItemTableLayoutPanel.RowCount = 4;
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.4898F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.4898F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.2653F));
+            this.ItemTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.7551F));
+            this.ItemTableLayoutPanel.Size = new System.Drawing.Size(150, 150);
+            this.ItemTableLayoutPanel.TabIndex = 0;
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DateLabel.Location = new System.Drawing.Point(3, 0);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(144, 36);
+            this.DateLabel.TabIndex = 0;
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TemperatureLabel.Location = new System.Drawing.Point(3, 72);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(144, 57);
+            this.TemperatureLabel.TabIndex = 2;
+            // 
+            // DesciptionLabel
+            // 
+            this.DesciptionLabel.AutoSize = true;
+            this.DesciptionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DesciptionLabel.Location = new System.Drawing.Point(3, 129);
+            this.DesciptionLabel.Name = "DesciptionLabel";
+            this.DesciptionLabel.Size = new System.Drawing.Size(144, 21);
+            this.DesciptionLabel.TabIndex = 3;
+            // 
+            // DailyItemUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ItemTableLayoutPanel);
+            this.Name = "DailyItemUserControl";
+            this.ItemTableLayoutPanel.ResumeLayout(false);
+            this.ItemTableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel ItemTableLayoutPanel;
+        private System.Windows.Forms.Label DesciptionLabel;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.Label DateLabel;
+    }
+}
