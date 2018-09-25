f34ef068c868390fcb4ca31c0563b5b7e6756c9a
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-25 16:29:44 +0400

    Added new exception RequestLimitationExcedeed

---
+++ WeatherApplication/Services/WeatherInfoGetter/Exceptions/RequestLimitationExcedeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Services.WeatherInfoGetter.Exceptions
+{
+    class RequestLimitationExcedeed: Exception
+    {
+        public RequestLimitationExcedeed(string message = "Request limitation was exceeded"):base(message)
+        {
+
+        }
+    }
+}
